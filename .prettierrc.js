e973067dc49eb6d37405e3f58a14a978a627bf5e
liuxiaobin <xiaobin.liu@karakal.com.cn>
2023-05-05 15:08:22 +0800

    feat: 增加依赖和exlint规则

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 120, // 单行最大长度限制
+    semi: true, // 句末加分号
+    singleQuote: true, // 单引号
+    trailingComma: 'all', // 尾逗号
+    bracketSpacing: true, // 对象字面量括号前后空格
+    arrowParens: 'always',
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'auto',
+    jsxBracketSameLine: false,
+    insertPragma: false,
+    requirePragma: false,
+};
